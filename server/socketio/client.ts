9598dabd8afd74e92aca8e9b7cf44d3ce678c806
TylerPrice7 <tyler101price@gmail.com>
2025-03-25 15:45:02 -0400

    feature: removed client.ts

--- server/socketio/client.ts
+++ server/socketio/client.ts
@@ -1,31 +1,31 @@
-import { io } from 'socket.io-client';
-import * as readline from 'readline';
+// import { io } from 'socket.io-client';
+// import * as readline from 'readline';
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-socket.on('connect', () => {
-  console.log('connected to server');
+// socket.on('connect', () => {
+//   console.log('connected to server');
 
-  const sendMessage = () => {
-    rl.question('Enter a message: ', (message) => {
-      socket.emit('message', message);
-      sendMessage();  
-    });
-  };
+//   const sendMessage = () => {
+//     rl.question('Enter a message: ', (message) => {
+//       socket.emit('message', message);
+//       sendMessage();  
+//     });
+//   };
 
-  sendMessage();
+//   sendMessage();
 
-  socket.on('message', (data) => {
-    console.log('message from server:', data);
-  });
-});
+//   socket.on('message', (data) => {
+//     console.log('message from server:', data);
+//   });
+// });
 
-socket.on('disconnect', () => {
-  console.log('disconnected from server');
-  rl.close();
-});
+// socket.on('disconnect', () => {
+//   console.log('disconnected from server');
+//   rl.close();
+// });
